d540f3870a423fb1db1fcf0276e5c7cfccfb0800
Nazmul Alom <nazmulshuvo03@gmail.com>
2021-11-16 11:39:01 +0600

    camera face switch added

--- src/components/QRCamera.js
+++ src/components/QRCamera.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import QrReader from "react-qr-reader";
 import { makeStyles } from "@material-ui/core/styles";
+import { CAMERA_FACE } from "../Constants";
+import { Button, Grid, Switch, Typography } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   qrSection: {
@@ -27,7 +29,17 @@ const useStyles = makeStyles((theme) => ({
 
 const QRCamera = () => {
   const classes = useStyles();
+  const [cameraOn, setCameraOn] = useState(false);
+  const [cameraFace, setCameraFace] = useState(CAMERA_FACE[1].value);
   const [result, setResult] = useState("No result");
+
+  const handleCameraFaceChange = (event) => {
+    if (event.target.checked) {
+      setCameraFace(CAMERA_FACE[1].value);
+    } else {
+      setCameraFace(CAMERA_FACE[0].value);
+    }
+  };
 
   const handleScan = (data) => {
     if (data) {
@@ -38,17 +50,53 @@ const QRCamera = () => {
   const handleError = (err) => {
     console.error(err);
   };
+  const handleLoad = (obj) => {
+    console.log("Camera loaded", obj);
+    obj.mirrorVideo = true;
+  };
 
   return (
-    <>
-      <QrReader
-        delay={300}
-        onError={handleError}
-        onScan={handleScan}
-        className={classes.qrSection}
-      />
+    <Grid container justifyContent="space-between" alignItems="center">
+      <Grid item>
+        {!cameraOn ? (
+          <Button
+            variant="outlined"
+            onClick={() => setCameraOn((prev) => !prev)}
+          >
+            Start
+          </Button>
+        ) : (
+          ""
+        )}
+      </Grid>
+      <Grid item>
+        <Typography variant="caption">Front</Typography>
+        <Switch
+          checked={cameraFace === CAMERA_FACE[1].value}
+          onChange={handleCameraFaceChange}
+          color="primary"
+          name="checkedB"
+          inputProps={{ "aria-label": "primary checkbox" }}
+        />
+        <Typography variant="caption">Back</Typography>
+      </Grid>
+      <Grid item xs={12}>
+        {cameraOn ? (
+          <QrReader
+            delay={300}
+            onError={handleError}
+            onScan={handleScan}
+            onLoad={handleLoad}
+            className={classes.qrSection}
+            facingMode={cameraFace}
+            showViewFinder={true}
+          />
+        ) : (
+          ""
+        )}
+      </Grid>
       <p>{result}</p>
-    </>
+    </Grid>
   );
 };
 
